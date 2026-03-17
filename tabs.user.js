1885ca054d84854407c1be37e7bfe47c76fe4f26
aj <134237782+ajtabjs@users.noreply.github.com>
2026-03-17 14:37:03 +0000

    change to use beginner format from a tutorial

--- tabs.user.js
+++ tabs.user.js
@@ -10,11 +10,10 @@
 // @description 3/17/2026, 6:53:32 AM
 // ==/UserScript==
 
-function wow(){
+(function() {
+    'use strict';
 
-var tab = document.getElementsByTagName("title")
+   var tab = document.getElementsByTagName("title")
 
 tab.textContent = "test";
-
-
-}
+})();
