69b9f93163dceb316e0e89b95f61803e1727f2e5
aj <134237782+ajtabjs@users.noreply.github.com>
2026-03-17 14:28:49 +0000

    init

---
+++ tabs.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        cool userscritp!!! totally not a bug
+// @namespace   aj
+// @match       *://*/*
+// @icon
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 3/17/2026, 6:53:32 AM
+// ==/UserScript==
+
+
